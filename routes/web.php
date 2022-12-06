bc64e7ac1927b0d8159400e572e1e42e09e59922
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-12-07 01:12:18 +0800

    week11: 利用Route::resource() 一次建立串接PhotoController七個方法的七個路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('photos', PhotoController::class);
 
 Route::get('/', function () {
     return view('welcome');
